9d86f1524e82737c0563dd3d66e9af2dfc6f7ae8
David Wheatley <davidwheatley03@gmail.com>
2020-06-10 17:07:45 +0100

    Update realme X2 Pro guide to v1.5

--- public/realme/js/script.js
+++ public/realme/js/script.js
@@ -51,3 +51,53 @@ document
       "https://dl.google.com/android/repository/platform-tools-latest-linux.zip"
     );
   });
+
+function randomiseDownloadLink(linkArray) {
+  const index = Math.floor(Math.random() * linkArray.length);
+
+  return linkArray[index];
+}
+
+// ColorOS China A.15
+document.querySelectorAll(".ozip-download--coloros__cn").forEach((link) => {
+  link.setAttribute(
+    "href",
+    randomiseDownloadLink([
+      "https://mega.nz/file/6MQSVaoI#uM-PVn1r570URXfGuJ73HGaWyLDVj9g-w7W7bcSfx4I",
+      "https://drive.google.com/file/d/1R7PlVbnmzUwclQR9-XM9McbuYWyMIdva/view?usp=sharing",
+    ])
+  );
+});
+
+// ColorOS Global A.10
+document.querySelectorAll(".ozip-download--coloros__global").forEach((link) => {
+  link.setAttribute(
+    "href",
+    randomiseDownloadLink([
+      "https://mega.nz/file/ORRWWSTL#McNOMXRBS8T3wiUuTaiwxFm4jKHfVa6_vgCE8rzDr-w",
+      "https://drive.google.com/file/d/1qWv1UsrVocCgMqvK2EbqMO11u34Rve7W/view?usp=sharing",
+    ])
+  );
+});
+
+// realmeme UI China C.25
+document.querySelectorAll(".ozip-download--realmemeui__cn").forEach((link) => {
+  link.setAttribute(
+    "href",
+    randomiseDownloadLink([
+      "https://mega.nz/file/aFR2QAaB#TvFkHoraHrxXdIxSEvaZwpZTpLjimV2oC0bTsAq-9II",
+      "https://drive.google.com/file/d/1iZG1QP9ix_av9vTD5GgjCgeSywTN9tWb/view?usp=sharing",
+    ])
+  );
+});
+
+// realmeme UI Global C.27
+document.querySelectorAll(".ozip-download--realmemeui__global").forEach((link) => {
+  link.setAttribute(
+    "href",
+    randomiseDownloadLink([
+      "https://mega.nz/file/CAB2SYLQ#Q_EXGe1apsUkYLPy_FzVumlI1x6hDPDweAOHHT_LO6U",
+      "https://drive.google.com/file/d/1S8WcOvgnLzTxgu6UZ0h2cJyKoQ6s0X7Z/view?usp=sharing",
+    ])
+  );
+});
