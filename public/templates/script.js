175e1cace8213dce526b52812dad790bf152c2c8
David Wheatley <hi@davwheat.dev>
2020-08-18 00:25:00 +0100

    Update answer templates

--- public/templates/script.js
+++ public/templates/script.js
@@ -784,8 +784,8 @@ If none of this works, you may need to [contact Apple Support](https://getsuppor
 |Service|Cost|
 |:---|:---:|
 |Data|20p/MB|
-|Calls & voicemail ^(\(charged per sec after 30s\))|£1/min|
-|Receiving calls ^(\(charged per min\))|£1/min|
+|Calls & voicemail ^(\\(charged per sec after 30s\\))|£1/min|
+|Receiving calls ^(\\(charged per min\\))|£1/min|
 |Sending texts|30p each|
 |Receiving texts|Free!|
 |MMS to UK/EU mobiles|30p each|
@@ -798,11 +798,97 @@ If none of this works, you may need to [contact Apple Support](https://getsuppor
 |Service|Cost|
 |:---|:---:|
 |Data|[color=red]£5/MB[/color]|
-|Calls & voicemail ^(\(charged per sec after 30s\))|£1/min|
-|Receiving calls ^(\(charged per min\))|£1/min|
+|Calls & voicemail ^(\\(charged per sec after 30s\\))|£1/min|
+|Receiving calls ^(\\(charged per min\\))|£1/min|
 |Sending texts|30p each|
 |Receiving texts|Free!|
 |MMS to UK/EU mobiles|30p each|
 |MMS to all other mobiles|24p each|`,
+  },
+  {
+    title: `5g-conspiracy`,
+    addFooter: true,
+    content: `
+
+5G is in the testing phase on giffgaff. If all goes to plan, we might see it switch on at the end of 2020 or early 2021. There's a small selection of 5G enabled devices available for sale from giffgaff.
+
+**There's no truth** in these conspiracy theories that 5G is dangerous.
+
+5G generally uses the same frequencies as 3G and 4G, using modern technology to add more data into these existing network standards.
+
+When people talk about the danger of 5G, they are referring to "mmWave" (milimetre wave) technology. mmWave is a high frequency network technology which only travels a few tens of metres from the mast. They claim that these high frequencies are dangerous to humans.
+
+WiFi routers use two main frequencies: 2.4 GHz and 5 GHz. These frequencies allow them to communicate with devices. 2.4 GHz supports lower speeds but can penetrate solid surfaces well. 5 GHz supports much faster speeds but can't travel through solid materials very well. These routers have been in use for almost two decades now, yet no-one has called them dangerous.
+
+5G (not mmWave) uses frequencies of up to 6 GHz. If there was any real danger in the main 5G networks, we would have already seen this from WiFi routers.
+
+mmWave technology uses frequencies between 24 GHz and 100 GHz. Scientists have researched these frequencies since the 80s, but have not found any dangers. They are already used in [airport scanners, astronomy, remote sensing, high speed microwave data links in the USA, weapons systems, and even modern medicine](https://en.wikipedia.org/wiki/Extremely_high_frequency#Applications). If we've already been using these frequencies for the past 40 years, we would have seen any dangerous side effects by now.
+
+To all the 5G conspiracy theorists: if you're going to start burning down 5G towers, start by destroying your WiFi router first, then go for all the military communication networks, and then modern medicine used for treating diseases. After that you can start burning down the towers.`,
+  },
+  {
+    title: 'mms broken',
+    addFooter: true,
+    content: `
+
+An issue with using the MMS (picture message) service was recognised by giffgaff and you can read more about it on the [Service Updates page](https://community.giffgaff.com/d/33109663-resolved-issues-using-mms-23-april-2020). Most of these cases are due to an incorrect "MMSC address" in your phone's settings.
+
+Make sure that all your APN settings (especially the MMSC address) are correct. The settings shown on the giffgaff help guides aren't complete. Press the button below to get the full list of settings
+
+[spoiler]
+
+If you use an iPhone, please follow this Apple Support guide to change your APN settings: https://support.apple.com/en-gb/HT201699
+
+If you use an Android device, head to Settings, Mobile Networks/Wireless & Networks, Mobile Network, APNs. After click the 'Add' or 'New' button (it may be hidden behind 3 dots).
+
+**Enter the APN values shown below.** Please note that some fields may not be shown on all devices, so you might not have to enter all of these.
+
+| Field Name | Field Value |
+|---|---|
+| Connection Name | giffgaff |
+| APN | \`giffgaff.com\` |
+| Proxy | [leave blank] |
+| Port | [leave blank] |
+| Username | \`giffgaff\` |
+| Password | [leave blank] |
+| Server | \`http://mmsc.mediamessaging.co.uk:8002\` |
+| MMSC | \`http://mmsc.mediamessaging.co.uk:8002\` |
+| MMS proxy | \`82.132.254.1\` |
+| MMS port | \`8080\` |
+| MCC | \`234\` |
+| MNC | \`10\` |
+| Authentication Type | \`PAP or CHAP\` |
+| APN type | \`default,supl,mms,ia,xcap\` |
+| APN protocol | \`IPv4\` |
+| APN roaming protocol | \`IPv4\` |
+| Bearer |  [leave blank] or \`Unspecified\` |
+| MVNO type |  \`SPN\` |
+| MVNO value |  \`giffgaff\` |
+[/spoiler]
+
+If you're still having issues, it's advised that you contact an agent who can investigate this problem more closely.
+
+You can contact a giffgaff agent at this link: https://support2.giffgaff.com/app/ask/Calls-texts-and-data/MMS-%28picture-messaging%29-not-working/form/
+
+Agents always aim to respond within 24 hours (in some busy periods it can take up to 48 hours). It usually only takes 3-5 hours. Agents work everyday between 8 am and 11 pm.
+
+When you get a response, you will receive a text and email alert telling you. You can view the response either from the bottom of your giffgaff dashboard, or by clicking this link: https://www.giffgaff.com/support/questions`,
+  },
+  {
+    title: 'balance notifications',
+    addFooter: true,
+    content: `
+
+A recent update to the way balance notifications are provided means that automated devices (like gates or alarms) may start forwarding these messages to your phone.
+
+These notifications used to be sent via an outdated system called USSD messaging, but now they're being sent by text instead. These devices are set up to automatically forward incoming texts to the owner's phone number. The forwarding of this text causes another balance notification to be sent, leading to an endless cycle of credit draining.
+
+You should turn off these balance notifications here: https://www.giffgaff.com/profile/details#notifications
+
+To get a refund, you should contact an agent which you can do so here: https://support2.giffgaff.com/app/ask/Top-up-balances-and-goodybags/Credit-debit-card-or-PayPal-overcharging/form/
+
+Agents always aim to respond within 24 hours (in some busy periods it can take up to 48 hours). It usually only takes 3-5 hours. Agents work everyday between 8 am and 11 pm.
+
+When you get a response, you will receive a text and email alert telling you. You can view the response either from the bottom of your giffgaff dashboard, or by clicking this link: https://www.giffgaff.com/support/questions`,
   },
 ];
